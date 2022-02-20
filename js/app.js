20c2b714425c420877309ec8a77bc114705a0187
nahiduzzaman2021 <akash.nahid.27@gmail.com>
2022-02-20 15:20:18 +0600

    tooltip done

---
+++ js/app.js
@@ -0,0 +1,5 @@
+// ToolTip JS
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
